6135432c67c680311186a7f524b55a290fa50e24
Joeri Nicolaes <joerinicolaes@gmail.com>
2018-01-12 10:06:12 +0100

    first dummy code to build sum up to certain int

---
+++ euler_friend_numbers.py
@@ -0,0 +1,18 @@
+import time
+
+def main():
+
+    N = 1e18
+
+    def sum(N):
+        start = time.time()
+        result = 0
+        for i in range(N):
+            if i % 1000:
+                print("** step %i **" %i)
+            result += i
+        end = time.time()
+        print("Sum till %N: %s, took %s seconds" %(N,result, end-start))
+        return result, end-start
+
+    sum(N)
